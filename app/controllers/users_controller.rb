fd1e73de46e2fa9ed16d45324be80bad7c2f4a0a
Ping <ping@nextacademy.com>
2016-12-27 18:38:30 +0000

    still not done with overide, (edit profile and a bit route prob)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < Clearance::UsersController
+    
+   def index
+      @user = User.all
+   end
+   
+   def show
+      @user = User.find(params[:id])
+   end
+
+   def new
+      @user = User.new
+   end
+  
+  
+   def create
+      @user = User.new(user_params)
+      
+      if @user.save
+         redirect_to users_path
+      else
+         
+         render 'new'
+      end
+   end
+     
+   def edit
+   end
+   
+   def update
+   end
+
+
+   private
+   def user_params
+      params.require(:user).permit(:username, :name, :email, :password)
+   end
+    
+end
